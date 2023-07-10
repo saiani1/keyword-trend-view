99b93aab3d28d61267a85eabbbad57010330bbe6
saiani1 <saiani@naver.com>
2023-07-10 21:50:50 +0900

    fix: api호출 시 CORS에러 해결

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
 import { IData } from "../types/types";
 
-const baseURL =
-  "https://openapi.naver.com/v1/datalab/shopping/category/keyword/age";
-
-export const getDataApi = async (data: IData) => {
-  const res = await axios.post(baseURL, data, {
-    headers: {
-      "X-Naver-Client-Id": process.env.REACT_APP_NAVER_CLIENT_ID,
-      "X-Naver-Client-Secret": process.env.REACT_APP_NAVER_CLIENT_SECRET,
-      "Content-Type": "application/json",
-    },
-  });
+export const getDataApi = async (params: IData) => {
+  const res = await axios.post(
+    "v1/datalab/shopping/category/keyword/age",
+    params,
+    {
+      headers: {
+        "X-Naver-Client-Id": process.env.REACT_APP_NAVER_CLIENT_ID,
+        "X-Naver-Client-Secret": process.env.REACT_APP_NAVER_CLIENT_SECRET,
+        "Content-Type": "application/json",
+      },
+    }
+  );
   return res;
 };
